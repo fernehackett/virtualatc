0663aa10e4a994045c2d81ab346c2668fdc088b8
Nic <tmninh94@gmail.com>
2021-11-23 01:14:20 +0700

    "Nic"

--- app/Jobs/SyncProducts.php
+++ app/Jobs/SyncProducts.php
@@ -65,6 +65,6 @@ class SyncProducts implements ShouldQueue
         $nextParams = array_merge($data, [
             "since_id" => $since_id
         ]);
-        self::dispatch($shopDomain, $nextParams);
+        self::dispatch($shop->getDomain()->toNative(), $nextParams);
     }
 }
